5eb1c2cde430f82149d405e244b96002cd5b4807
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-02 16:15:06 +0800

    chore: adjust Vite configuration settings

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,37 @@
 import { defineConfig } from "vite";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 export default defineConfig({
     plugins: [
+        Components({
+            resolvers: [
+                AntDesignVueResolver({
+                    importStyle: false, // css in js
+                }),
+            ],
+        }),
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-    resolve: (name) => {
+    resolve: {
         alias: {
-            'ziggy-js': path.resolve('vendor/tightenco/ziggy'),
+            "@": path.resolve(__dirname, "resources/js"),
+            assets: path.resolve(__dirname, "public/assets"),
+            "ziggy-js": path.resolve(__dirname, "vendor/tightenco/ziggy"),
         },
     },
-})
+});
