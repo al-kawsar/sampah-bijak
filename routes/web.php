141ed9a6daa50b1f3ca2ab16ec43bf81890f04b9
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-02 22:35:44 +0800

    feat: update routes to include new controllers and functionalities

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Home\HomeController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return "Halaman Dashboard";
+Route::name('home.')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('index');
+});
+
+Route::name('auth.')->prefix('auth')->group(function () {
+    Route::controller(LoginController::class)->group(function () {
+        Route::get('/login', 'showLoginForm')->name('login.index');
+        Route::post('/login', 'doLogin')->name('login.submit');
+    });
+    Route::controller(RegisterController::class)->group(function () {
+        Route::get('/register', 'showRegisterForm')->name('register.index');
+        Route::post('/register', 'doRegister')->name('register.submit');
+    });
+});
+
+
+Route::name('app.')->middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('App/Dashboard');
+    })->name('dashboard');
+
 });
