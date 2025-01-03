370828310be21ee605a05d23060029c0d7753bc3
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-03 13:49:05 +0800

    style(app.blade.php): clean up head section

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <meta name="theme-color" content="#ffffff">
 
-    <link rel="icon" type="image/png" sizes="196x196" href="/images/favicon-196.png">
-    <link rel="apple-touch-icon" href="/images/pwa-192x192.png">
-
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="Your App Name">
-
+    <!-- Laravel Inertia & Vite -->
     @routes
     @vite(['resources/js/app.js'])
     @inertiaHead
