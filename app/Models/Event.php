333e14ed4cf3517f307b3631c83c8efde88eb592
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-03 23:24:18 +0800

    feat: implement UUID generation for Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,4 +9,20 @@ class Event extends Model
 {
     /** @use HasFactory<\Database\Factories\EventFactory> */
     use HasFactory;
+
+    public $incrementing = false; // Disable auto increment
+    protected $keyType = 'string';
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                // Generate UUID v4 for the ID if it's not set
+                $model->id = (string) str()->uuid(); // Use uuid4 for unique ID
+            }
+        });
+    }
 }
