aad67591eb2765f0b4f45e27bc924be79809fbde
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-03 23:24:19 +0800

    feat: implement UUID generation for LearningModule model

--- app/Models/LearningModule.php
+++ app/Models/LearningModule.php
@@ -9,4 +9,18 @@ class LearningModule extends Model
 {
     /** @use HasFactory<\Database\Factories\LearningModuleFactory> */
     use HasFactory;
+    public $incrementing = false; // Disable auto increment
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                // Generate UUID v4 for the ID if it's not set
+                $model->id = (string) str()->uuid(); // Use uuid4 for unique ID
+            }
+        });
+    }
 }
