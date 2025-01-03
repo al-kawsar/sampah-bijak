7cb425388b25c9bae7b12c41351c965cc5c32d41
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-03 23:24:21 +0800

    feat: implement UUID generation for PickupSchedule model

--- app/Models/PickupSchedule.php
+++ app/Models/PickupSchedule.php
@@ -9,4 +9,18 @@ class PickupSchedule extends Model
 {
     /** @use HasFactory<\Database\Factories\PickupScheduleFactory> */
     use HasFactory;
+    public $incrementing = false; // Disable auto increment
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                // Generate UUID v4 for the ID if it's not set
+                $model->id = (string) str()->uuid(); // Use uuid4 for unique ID
+            }
+        });
+    }
 }
