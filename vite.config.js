b2ec55fb8ba06fa738e09d7da2bc2ce54fe784dd
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-03 13:49:16 +0800

    chore(vite.config.js): remove PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import { VitePWA } from 'vite-plugin-pwa';
 import path from "path";
 
 export default defineConfig({
@@ -26,33 +25,6 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
-        }),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-                name: 'Sampah Bijak',
-                short_name: 'App',
-                theme_color: '#ffffff',
-                icons: [
-                    {
-                        src: '/images/192.png',
-                        sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        src: '/images/512.png',
-                        sizes: '512x512',
-                        type: 'image/png'
-                    }
-                ],
-                start_url: '/',
-                display: 'standalone',
-                background_color: '#ffffff'
-            },
-            workbox: {
-                navigateFallback: '/',
-                globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
-            }
         }),
     ],
     resolve: {
