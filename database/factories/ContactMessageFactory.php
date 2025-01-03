42662bd5958341eae1e7fedb7f4466819847e53a
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-03 23:24:32 +0800

    feat: enhance ContactMessageFactory with additional fields

--- database/factories/ContactMessageFactory.php
+++ database/factories/ContactMessageFactory.php
@@ -17,7 +17,11 @@ class ContactMessageFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'subject' => $this->faker->sentence,
+            'message' => $this->faker->paragraph,
+            'status' => $this->faker->randomElement(['unread', 'read', 'replied']),
         ];
     }
 }
