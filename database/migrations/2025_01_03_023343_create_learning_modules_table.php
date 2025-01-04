bcdb8a75ccfe2c481b4f64acdee73092d508f811
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-04 20:21:19 +0800

    refactor: change description column type to text in learning modules migration

--- database/migrations/2025_01_03_023343_create_learning_modules_table.php
+++ database/migrations/2025_01_03_023343_create_learning_modules_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,7 @@ return new class extends Migration
         Schema::create('learning_modules', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->text('content');
             $table->string('thumbnail');
             $table->timestamps();
