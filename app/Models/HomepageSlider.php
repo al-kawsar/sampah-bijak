a8dea311a1a34d0c09b5473ff9c7b7fcf08a166c
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-04 20:19:47 +0800

    feat: add fillable and casts properties in HomepageSlider model

--- app/Models/HomepageSlider.php
+++ app/Models/HomepageSlider.php
@@ -9,4 +9,21 @@ class HomepageSlider extends Model
 {
     /** @use HasFactory<\Database\Factories\HomepageSliderFactory> */
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'link',
+        'order_number',
+        'is_active',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
 }
