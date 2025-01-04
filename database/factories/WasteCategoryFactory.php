31dafe1dbc82afea2e374761fb27d454dbfc5c42
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-04 20:21:14 +0800

    feat: add default values in WasteCategoryFactory

--- database/factories/WasteCategoryFactory.php
+++ database/factories/WasteCategoryFactory.php
@@ -17,7 +17,8 @@ class WasteCategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence,
         ];
     }
 }
