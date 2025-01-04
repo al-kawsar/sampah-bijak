077f91e426806dcc6aa1c918a1242203d49f57b6
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-04 20:19:38 +0800

    feat: add fillable and casts properties in ContactMessage model

--- app/Models/ContactMessage.php
+++ app/Models/ContactMessage.php
@@ -9,4 +9,19 @@ class ContactMessage extends Model
 {
     /** @use HasFactory<\Database\Factories\ContactMessageFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'email', 
+        'subject', 
+        'message', 
+        'status'
+    ];
+
+    // Mengatur tipe data kolom dalam model
+    protected $casts = [
+        'status' => 'string', // Status sudah dalam bentuk enum, bisa cast ke string
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 }
