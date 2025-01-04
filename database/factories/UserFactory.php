e2ee6717c41a76dc36c101917490fce9b2a12ada
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-04 20:21:10 +0800

    feat: add default values in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'username' => $this->faker->unique()->userName,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'), // Default password
+            'region_id' => \App\Models\Region::factory(),
+            'role_id' => \App\Models\Role::inRandomOrder()->first()->id,
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
