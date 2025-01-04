abbea03f7cfc7e9a12b1fb8f2331a90964869dd0
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-04 20:20:11 +0800

    feat: add fillable and relationship methods in UserProfile model

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -9,4 +9,18 @@ class UserProfile extends Model
 {
     /** @use HasFactory<\Database\Factories\UserProfileFactory> */
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'full_name',
+        'phone_number',
+        'profile_picture',
+    ];
+
+    /**
+     * Define relationship with User model.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
