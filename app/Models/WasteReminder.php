aa2e9fb0efebd55b427cde687d1db620cb14d420
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-04 20:20:39 +0800

    feat: add fillable properties in WasteReminder model

--- app/Models/WasteReminder.php
+++ app/Models/WasteReminder.php
@@ -9,4 +9,12 @@ class WasteReminder extends Model
 {
     /** @use HasFactory<\Database\Factories\WasteReminderFactory> */
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'reminder_time',
+        'frequency',
+        'is_active',
+    ];
 }
