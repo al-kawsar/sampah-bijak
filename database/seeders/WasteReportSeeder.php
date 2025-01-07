363d2889c8eccc7e902eabbd2c737a30a58e95dc
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-07 22:56:32 +0800

    feat: add sample waste reports in WasteReportSeeder

--- database/seeders/WasteReportSeeder.php
+++ database/seeders/WasteReportSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\WasteReport;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class WasteReportSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        WasteReport::factory()->count(10)->create();
     }
 }
