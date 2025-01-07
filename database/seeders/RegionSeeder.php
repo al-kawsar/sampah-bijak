8e2afcfac2a20c8bf477842b149fb4b2ebdfc0a1
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-07 22:56:22 +0800

    feat: add sample regions in RegionSeeder

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Region;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,10 @@ class RegionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Region::create(['region_name' => 'Jakarta']);
+        Region::create(['region_name' => 'Bandung']);
+        Region::create(['region_name' => 'Surabaya']);
+        Region::create(['region_name' => 'Medan']);
+        Region::create(['region_name' => 'Bali']);
     }
 }
