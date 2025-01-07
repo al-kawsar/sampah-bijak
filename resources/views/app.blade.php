ad75564921122083ec8e6670ad32389f5ed20511
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-07 22:56:35 +0800

    fix: update app.blade.php for PWA support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
+    {{-- <meta name="mobile-web-app-capable" content="yes"> --}}
+    <link rel="shortcut icon" href="{{ asset('logo.png') }}" type="image/x-icon">
+
+    <!-- PWA  -->
+    {{-- <meta name="theme-color" content="#6777ef" /> --}}
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
 
     <!-- Laravel Inertia & Vite -->
     @routes
@@ -13,6 +21,8 @@
 
 <body>
     @inertia
+
+    {{-- <script src="{{ asset('sw.js') }}"></script> --}}
 </body>
 
 </html>
