43d4ce029b0a0efcefa234097b0cb15ca1ad5097
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-08 04:48:46 +0800

    refactor: update routes for better organization and add resource routes

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,82 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
+
 use App\Http\Controllers\Home\HomeController;
+
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\ContactMessageController;
+use App\Http\Controllers\Dashboard\HomepageSliderController;
+use App\Http\Controllers\Dashboard\FAQController;
+use App\Http\Controllers\Dashboard\EventController;
+use App\Http\Controllers\Dashboard\LearningModuleController;
+use App\Http\Controllers\Dashboard\NotificationController;
+use App\Http\Controllers\Dashboard\PickupScheduleController;
+use App\Http\Controllers\Dashboard\RegionController;
+use App\Http\Controllers\Dashboard\RoleController;
+use App\Http\Controllers\Dashboard\UserController;
+use App\Http\Controllers\Dashboard\WasteCategoryController;
+use App\Http\Controllers\Dashboard\WasteItemController;
+use App\Http\Controllers\Dashboard\WasteLocationController;
+use App\Http\Controllers\Dashboard\WasteReminderController;
+use App\Http\Controllers\Dashboard\WasteReportController;
+use App\Http\Controllers\Dashboard\SettingController;
+
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::name('home.')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('index');
 });
 
 Route::name('auth.')->prefix('auth')->middleware('guest')->group(function () {
-    Route::controller(LoginController::class)->group(function () {
-        Route::get('/login', 'showLoginForm')->name('login.index');
-        Route::post('/login', 'doLogin')->name('login.submit');
+    Route::controller(LoginController::class)->prefix('login')->group(function () {
+        Route::get('/', 'showLoginForm')->name('login.index');
+        Route::post('/', 'doLogin')->name('login.submit');
     });
-    Route::controller(RegisterController::class)->group(function () {
-        Route::get('/register', 'showRegisterForm')->name('register.index');
-        Route::post('/register', 'doRegister')->name('register.submit');
+    Route::controller(RegisterController::class)->prefix('register')->group(function () {
+        Route::get('/', 'showRegisterForm')->name('register.index');
+        Route::post('/', 'doRegister')->name('register.submit');
     });
+
+    Route::match(['get', 'post'], 'logout', [LogoutController::class, 'logout'])->WithoutMiddleware('guest')->middleware('auth')->name('logout');
+
 });
 
 
 Route::name('app.')->middleware('auth')->group(function () {
 
-    Route::get('/dashboard', function () {
-        return Inertia::render('App/Dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+    require_once __DIR__ . '/app/api.php';
+
+
+    // * HOMEPAGE API
+    Route::resource('contacts', ContactMessageController::class)->parameters(['contacts' => 'id:id']);
+    Route::resource('sliders', HomepageSliderController::class)->parameters(['sliders' => 'id:id']);
+    Route::resource('faqs', FAQController::class)->parameters(['faqs' => 'id:id']);
+
+    // * DASHBOARD API
+    Route::resource('events', EventController::class)->parameters(['events' => 'id:id']);
+    Route::resource('learnings', LearningModuleController::class)->parameters(['learnings' => 'id:id']);
+    Route::resource('notifications', NotificationController::class)->parameters(['notifications' => 'id:id']);
+    Route::resource('pickup-schedules', PickupScheduleController::class)->parameters(['pickup-schedules' => 'id:id']);
+    Route::resource('regions', RegionController::class)->parameters(['regions' => 'id:id']);
+    Route::resource('roles', RoleController::class)->parameters(['roles' => 'id:id']);
+    Route::resource('users', UserController::class)->parameters(['users' => 'id:id']);
+    Route::resource('waste-categories', WasteCategoryController::class)->parameters(['waste-categories' => 'id:id']);
+    Route::resource('waste-items', WasteItemController::class)->parameters(['waste-items' => 'id:id']);
+    Route::resource('waste-locations', WasteLocationController::class)->parameters(['waste-locations' => 'id:id']);
+    Route::resource('waste-reminders', WasteReminderController::class)->parameters(['waste-reminders' => 'id:id']);
+    Route::resource('waste-reports', WasteReportController::class)->parameters(['waste-reports' => 'id:id']);
+
+
+    Route::name('settings.')->controller(SettingController::class)->prefix('settings')->group(function () {
+        Route::get('profile', 'profilePage')->name('profile');
+        Route::put('profile/{id:id}', 'updateProfile')->name('profile.update');
+        Route::get('account', 'accountPage')->name('account');
+        Route::get('account/{id:id}', 'updateAccount')->name('account.update');
+    });
+
 
 });
