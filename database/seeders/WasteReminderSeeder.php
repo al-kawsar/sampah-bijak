43ba7f1c0b5df511f0b6ac7839cf6c7e1bf350b6
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-07 22:56:29 +0800

    feat: add sample waste reminders in WasteReminderSeeder

--- database/seeders/WasteReminderSeeder.php
+++ database/seeders/WasteReminderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\WasteReminder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class WasteReminderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        WasteReminder::factory()->create(['title' => 'Weekly Waste Pickup', 'description' => 'Reminder for weekly waste pickup.', 'reminder_time' => '08:00:00', 'frequency' => 'weekly', 'is_active' => true]);
+        WasteReminder::factory()->create(['title' => 'Monthly Recycling Reminder', 'description' => 'Reminder for monthly recycling.', 'reminder_time' => '09:00:00', 'frequency' => 'monthly', 'is_active' => true]);
     }
 }
