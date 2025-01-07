2b1c516ed9236d4b4087e910a0f7ad7b70d8207b
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-07 22:56:12 +0800

    feat: add sample data in ContactMessageSeeder

--- database/seeders/ContactMessageSeeder.php
+++ database/seeders/ContactMessageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ContactMessage;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class ContactMessageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // ContactMessage::factory()->create(['name' => 'John Doe', 'email' => 'john@example.com', 'subject' => 'Inquiry', 'message' => 'I would like to know more about your services.', 'status' => 'unread']);
+        // ContactMessage::factory()->create(['name' => ' Jane Smith', 'email' => 'jane@example.com', 'subject' => 'Feedback', 'message' => 'Great service! Keep it up.', 'status' => 'read']);
     }
 }
