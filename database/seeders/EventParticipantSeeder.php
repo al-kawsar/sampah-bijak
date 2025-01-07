4a1ac0c9af837332bde57651bac3718e8fde7650
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-07 22:56:14 +0800

    feat: add sample data in EventParticipantSeeder

--- database/seeders/EventParticipantSeeder.php
+++ database/seeders/EventParticipantSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\EventParticipant;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class EventParticipantSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        EventParticipant::factory()->count(10)->create();
     }
 }
