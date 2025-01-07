9b754177127e5c3cfd08fa1f85c3ae305329e63e
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-07 22:56:19 +0800

    feat: add sample data in HomepageSliderSeeder

--- database/seeders/HomepageSliderSeeder.php
+++ database/seeders/HomepageSliderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\HomepageSlider;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,9 @@ class HomepageSliderSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        // HomepageSlider::factory()->create(['title' => 'Welcome to Waste Management', 'description' => 'Join us in making a cleaner environment.', 'image' => 'slider1.jpg', 'link' => 'https://example.com']);
+        // HomepageSlider::factory()->create(['title' => 'Get Involved', 'description' => 'Learn how you can help.', 'image' => 'slider2.jpg', 'link' => 'https://example.com']);
     }
 }
