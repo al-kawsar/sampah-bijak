13a7a2f47b87f2ea24bc69e22d75ec6b450a613f
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-08 18:52:40 +0800

    config(vue): update app initialization with Antd

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import { ZiggyVue } from "ziggy-js";
 import { Link, Head } from "@inertiajs/vue3";
+import Antd, { message } from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
 
 createInertiaApp({
@@ -16,11 +17,16 @@ createInertiaApp({
         return pages[`./Pages/${name}.vue`];
     },
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .component("Link", Link)
-            .component("Head", Head)
-            .mount(el);
+        const app = createApp({
+            render: () => h(App, props),
+        });
+
+        app.use(plugin);
+        app.use(ZiggyVue);
+        app.use(Antd);
+        app.component("Link", Link);
+        app.component("Head", Head);
+        app.mount(el);
+
     },
 });
