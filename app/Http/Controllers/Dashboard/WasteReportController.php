4937ecf61508b173feb2a7fcd9fc6ad58c0c22c5
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-09 00:00:00 +0800

    feat: add waste report management

--- app/Http/Controllers/Dashboard/WasteReportController.php
+++ app/Http/Controllers/Dashboard/WasteReportController.php
@@ -11,6 +11,7 @@ use App\Models\WasteReport;
 use Inertia\Inertia;
 use App\Traits\ApiResponse;
 use App\Traits\PaginatesData;
+use Illuminate\Support\Facades\Log;
 class WasteReportController extends Controller
 {
     use ApiResponse, PaginatesData;
@@ -25,16 +26,16 @@ class WasteReportController extends Controller
         switch ($role) {
             case 'warga':
             case 'petugas':
-                return $this->WasteReportPage();
+            return $this->WasteReportPage();
             case 'admin':
             case 'pemerintah':
-                return $this->WasteReportManagement();
+            return $this->WasteReportManagement();
         }
     }
     public function getData(Request $request)
     {
         try {
-            $query = $request->input('search');
+            $query = $request->input('search', '');
             $limit = $request->input('limit', 10);
             $type = $request->input('type', 'search');
 
@@ -43,23 +44,21 @@ class WasteReportController extends Controller
                 $cacheKey = self::PAGE . '_search:' . md5($query);
 
                 $results = Cache::remember($cacheKey, 60, function () use ($query, $limit) {
-                    return WasteReport::where('username', 'LIKE', "%$query%")
-                        ->orWhere('email', 'LIKE', "%$query%")
-                        ->orderBy('id', 'desc')
-                        ->paginate($limit);
+                    return WasteReport::where('description', 'LIKE', "%$query%")
+                    ->orderBy('updated_at', 'desc')
+                    ->paginate($limit);
                 });
 
                 return $this->success($results->items(), "Get Search Data", pagination: $this->getPaginationData($results));
-
             }
 
             $data = WasteReport::with([
-                'user:id,email',                      // Relasi user
-                'user.profile',                      // Relasi bersarang: role dari user
-                'location:id,name,address',              // Relasi location
-                'category:id,name'                       // Relasi category
+                'user:id,email',
+                'user.profile',
+                'location:id,name,address',
+                'category:id,name'
             ])
-                ->orderBy('category_id', 'desc')->paginate($limit);
+            ->orderBy('updated_at', 'desc')->paginate($limit);
 
 
             return $this->success($data->items(), "Get All Data", pagination: $this->getPaginationData($data));
@@ -92,6 +91,13 @@ class WasteReportController extends Controller
     {
         return Inertia::render("App/Management/WasteReport/Show", [
             "waste_report" => $id
+        ]);
+    }
+
+    public function edit(WasteReport $id)
+    {
+        return Inertia::render("App/Management/WasteReport/Edit", [
+            "wasteReport" => $id
         ]);
     }
 
