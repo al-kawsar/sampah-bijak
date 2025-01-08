27e601511a5a98f52065127c0e79f5cee0a5ec6a
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-08 18:52:35 +0800

    feat(seeder): add pickup schedule seed data

--- database/seeders/PickupScheduleSeeder.php
+++ database/seeders/PickupScheduleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PickupSchedule;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class PickupScheduleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        PickupSchedule::factory()->create(['pickup_date' => '2023-10-01', 'pickup_time' => '10:00:00', 'notes' => 'Please pick up from the front door.', 'status' => 'pending']);
+        PickupSchedule::factory()->create(['pickup_date' => '2023-10-02', 'pickup_time' => '11:00:00', 'notes' => 'Pickup from the backyard.', 'status' => 'confirmed']);
     }
 }
