b76674bdcf1117a785b579fe583e3cace5ce5631
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-14 09:57:06 +0800

    refactor: reorganize middleware configuration in bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,57 +11,53 @@ use Inertia\Inertia;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
+->withRouting(
+    web: __DIR__ . '/../routes/web.php',
+    commands: __DIR__ . '/../routes/console.php',
+    health: '/up',
+)
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->redirectGuestsTo(function (Request $request) {
+        return route('auth.login.index');
+    });
 
-        $middleware->web(append: [
-            HandleInertiaRequests::class,
-        ]);
+    $middleware->statefulApi();
 
-        $middleware->alias([
-            'role' => checkRole::class,
-            'LaravelPwa' => \Ladumor\LaravelPwa\LaravelPwa::class,
-        ]);
+    $middleware->web(append: [
+        HandleInertiaRequests::class,
+    ]);
+
+    $middleware->alias([
+        'role' => checkRole::class,
+        'LaravelPwa' => \Ladumor\LaravelPwa\LaravelPwa::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+    ]);
 
         // $middleware->validateCsrfTokens(except: [
         // ]);
-    
-        $middleware->redirectGuestsTo(function (Request $request) {
-            return route('auth.login.index');
-        });
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
-                return Inertia::render('Errors/Index', [
-                    'status' => $response->getStatusCode()
-                ])
-                    ->toResponse($request)
-                    ->setStatusCode($response->getStatusCode());
-            } elseif (in_array($response->getStatusCode(), [500, 503, 404, 403])) {
-                // return Inertia::render('Errors/Index', [
-                //     'status' => $response->getStatusCode()
-                // ])
-                //     ->toResponse($request)
-                //     ->setStatusCode($response->getStatusCode());
-                return redirect()->back()->withErrors([
-                    'message' => 'Anda tidak memiliki hak akses yang cukup untuk masuk ke halaman yang anda tuju.'
-                ]);
-            }
+})
+->withExceptions(function (Exceptions $exceptions) {
+    $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+        if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+            return Inertia::render('Errors/Index', [
+                'status' => $response->getStatusCode()
+            ])
+            ->toResponse($request)
+            ->setStatusCode($response->getStatusCode());
+        } elseif ($response->getStatusCode() === 419) {
+            return back()->with([
+                'message' => 'The page expired, please try again.',
+            ]);
+        }
 
-            return $response;
-        });
-        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'message' => 'Record not found.'
-                ], 404);
-            }
-        });
+        return $response;
+    });
+    $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+        if ($request->is('api/*')) {
+            return response()->json([
+                'message' => 'Record not found.'
+            ], 404);
+        }
+    });
 
-    })->create();
+})->create();
