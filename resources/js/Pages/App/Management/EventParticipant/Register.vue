3e71b39a81b28c11ce0ee0a95874362fdbc4675f
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-16 08:54:32 +0800

    feat: add registration component for event participants

---
+++ resources/js/Pages/App/Management/EventParticipant/Register.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import AppLayout from "@/Layouts/AppLayout.vue";
+  import { ref, onMounted, h } from "vue";
+  import { router } from "@inertiajs/vue3";
+
+  defineOptions({ layout: AppLayout });
+</script>
+<template>
+
+    Halaman Registrasi Partisipasi Acara
+</template>
